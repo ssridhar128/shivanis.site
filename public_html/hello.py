374573a900d0ff9113a6823729d678889fcc9b9c
ssridhar128 <79930825+ssridhar128@users.noreply.github.com>
2026-01-31 11:45:08 -0800

    This is me sanity checking python cgi

---
+++ public_html/hello.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+print("Content-Type: text/html")
+print()  # Crucial blank line
+print("<html><body>")
+print("<h1>Shivani's Python CGI is Working!</h1>")
+print("</body></html>")
