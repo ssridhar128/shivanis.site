e54ed4e0dd831049b2139ac067ddbcf69fc373e0
ssridhar128 <79930825+ssridhar128@users.noreply.github.com>
2026-01-31 17:33:03 -0800

    Fixing the changing owner

--- public_html/hw2/hello.py
+++ public_html/hw2/hello.py
@@ -2,5 +2,5 @@
 print("Content-Type: text/html")
 print()  # Crucial blank line
 print("<html><body>")
-print("<h1>Shivani's Python CGI is Working!</h1>")
+print("<h1>Shivani Python CGI is Working!</h1>")
 print("</body></html>")
--- public_html/hw2/hello.rb
+++ public_html/hw2/hello.rb
@@ -3,7 +3,7 @@ puts "Content-type: text/html"
 puts "" # The mandatory blank line
 puts "<html>"
 puts "<body>"
-puts "<h1>Ruby CGI Lives!</h1>"
+puts "<h1>Shivani Ruby CGI Lives!</h1>"
 puts "  <p>The time is: #{Time.now}</p>"
 puts "</body>"
 puts "</html>"
