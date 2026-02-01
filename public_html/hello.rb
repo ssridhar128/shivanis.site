dd5a119e3b7cb62925308bce7cb10c4f81498de6
ssridhar128 <79930825+ssridhar128@users.noreply.github.com>
2026-01-31 16:33:47 -0800

    This is my check for the ruby sanity check

---
+++ public_html/hello.rb
@@ -0,0 +1,9 @@
+#!/usr/bin/ruby
+puts "Content-type: text/html"
+puts "" # The mandatory blank line
+puts "<html>"
+puts "<body>"
+puts "<h1>Ruby CGI Lives!</h1>"
+puts "  <p>The time is: #{Time.now}</p>"
+puts "</body>"
+puts "</html>"
